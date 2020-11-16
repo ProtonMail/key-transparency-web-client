648a1d5b3fb106e756677cc463a2c7528b5d85dc
Marco <marco.martinoli@protonmail.com>
2020-11-16 14:58:42 +0100

    Add tests for certificate transparency

--- test/certTransparency.spec.js
+++ test/certTransparency.spec.js
@@ -1,3 +1,4 @@
+import * as asn1js from "asn1js";
 import { epoch } from "./keyTransparency.data";
 import {
   parseCertificate,
@@ -14,5 +15,164 @@ describe("certificate transparency", () => {
     checkAltName(cert, ChainHash, EpochID);
     await verifyLEcert(cert);
     await verifySCT(cert);
+  });
+
+  it("should fail to parse with corrupt certificate", () => {
+    let errorThrown = true;
+    try {
+      parseCertificate("corrupt");
+      errorThrown = false;
+    } catch (err) {
+      expect(err.message).toEqual(
+        "Object's schema was not verified against input data for Certificate"
+      );
+    }
+    expect(errorThrown).toEqual(true);
+  });
+
+  it("should fail in checkAltName with missing extensions", () => {
+    const { Certificate, ChainHash, EpochID } = epoch;
+    const cert = parseCertificate(Certificate);
+    const { extensions, ...certNoExt } = cert;
+
+    let errorThrown = true;
+    try {
+      checkAltName(certNoExt, ChainHash, EpochID);
+      errorThrown = false;
+    } catch (err) {
+      expect(err.message).toEqual("Epoch certificate does not have extensions");
+    }
+    expect(errorThrown).toEqual(true);
+  });
+
+  it("should fail in checkAltName with missing AltName extension", () => {
+    const { Certificate, ChainHash, EpochID } = epoch;
+    const cert = parseCertificate(Certificate);
+    const corruptExt = cert.extensions.filter(
+      (ext) => ext.extnID !== "2.5.29.17"
+    );
+
+    let errorThrown = true;
+    try {
+      checkAltName({ ...cert, extensions: corruptExt }, ChainHash, EpochID);
+      errorThrown = false;
+    } catch (err) {
+      expect(err.message).toEqual(
+        "Epoch certificate does not have AltName extension"
+      );
+    }
+    expect(errorThrown).toEqual(true);
+  });
+
+  it("should fail in checkAltName with corrupt altName", () => {
+    const { Certificate, ChainHash, EpochID } = epoch;
+    const cert = parseCertificate(Certificate);
+    cert.extensions.map((ext) => {
+      if (ext.extnID === "2.5.29.17") {
+        ext.parsedValue.altNames[0].value = "corrupt";
+      }
+      return ext;
+    });
+
+    let errorThrown = true;
+    try {
+      checkAltName(cert, ChainHash, EpochID);
+      errorThrown = false;
+    } catch (err) {
+      expect(err.message).toEqual(
+        "Epoch certificate alternative name does not match"
+      );
+    }
+    expect(errorThrown).toEqual(true);
+  });
+
+  it("should fail certificate verification", async () => {
+    const { Certificate } = epoch;
+    const cert = parseCertificate(Certificate);
+    cert.tbs = new Uint8Array(10).buffer;
+
+    let errorThrown = true;
+    try {
+      await verifyLEcert(cert);
+      errorThrown = false;
+    } catch (err) {
+      expect(err.message).toEqual(
+        "Epoch certificate did not pass verification against issuer's public key"
+      );
+    }
+    expect(errorThrown).toEqual(true);
+  });
+
+  it("should fail in verifySCT with missing extensions", async () => {
+    const { Certificate } = epoch;
+    const cert = parseCertificate(Certificate);
+    const { extensions, ...certNoExt } = cert;
+
+    let errorThrown = true;
+    try {
+      await verifySCT(certNoExt);
+      errorThrown = false;
+    } catch (err) {
+      expect(err.message).toEqual(
+        `SCT verification halted with error "Cannot read property 'length' of undefined"`
+      );
+    }
+    expect(errorThrown).toEqual(true);
+  });
+
+  it("should fail in verifySCT with missing SCTs extension", async () => {
+    const { Certificate } = epoch;
+    const cert = parseCertificate(Certificate);
+    const corruptExt = cert.extensions.filter(
+      (ext) => ext.extnID !== "1.3.6.1.4.1.11129.2.4.2"
+    );
+
+    let errorThrown = true;
+    try {
+      await verifySCT({ ...cert, extensions: corruptExt });
+      errorThrown = false;
+    } catch (err) {
+      expect(err.message).toEqual(
+        `SCT verification halted with error "No SignedCertificateTimestampList extension in the specified certificate"`
+      );
+    }
+    expect(errorThrown).toEqual(true);
+  });
+
+  it("should fail in verifySCT with missing SCTs", async () => {
+    const { Certificate } = epoch;
+    const cert = parseCertificate(Certificate);
+    cert.extensions.map((ext) => {
+      if (ext.extnID === "1.3.6.1.4.1.11129.2.4.2") {
+        ext.parsedValue.timestamps = [];
+      }
+      return ext;
+    });
+
+    let errorThrown = true;
+    try {
+      await verifySCT(cert);
+      errorThrown = false;
+    } catch (err) {
+      expect(err.message).toEqual(
+        `SCT verification halted with error "Nothing to verify in the certificate"`
+      );
+    }
+    expect(errorThrown).toEqual(true);
+  });
+
+  it("should fail in verifySCT with corrupt certificate", async () => {
+    const { Certificate } = epoch;
+    const cert = parseCertificate(Certificate);
+    cert.serialNumber = new asn1js.Integer();
+
+    let errorThrown = true;
+    try {
+      await verifySCT(cert);
+      errorThrown = false;
+    } catch (err) {
+      expect(err.message).toEqual("SCT verification failed");
+    }
+    expect(errorThrown).toEqual(true);
   });
 });
